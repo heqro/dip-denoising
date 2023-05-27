5b87238e590c2d8bb86ec6a989fdb6857af7f0af
Héctor Iglesias <hr.iglesias.2018@alumnos.urjc.es>
2023-05-27 18:49:36 +0200

    Add demo and update _utils to better reflect the utility of the 'save_image' function

--- _utils.py
+++ _utils.py
@@ -16,9 +16,14 @@ def add_gaussian_noise(img, avg: float, std: float) -> torch.Tensor:
     return img + np.random.normal(avg, std, img.shape).astype('float32')
 
 
-def save_image(img: torch.Tensor, file_name: str = "", show=False):
+def print_image(img: torch.Tensor, file_name: str = "", show=False):
     import matplotlib.pyplot as plt
     img_transposed = img.transpose(1, 2, 0)
     fig = plt.figure()
-    plt.imsave(fname=file_name, arr=img_transposed, format="png")
+    if file_name != "":
+        plt.imsave(fname=file_name, arr=img_transposed, format="png")
+    else:
+        plt.imshow(img_transposed)
+        plt.axis('off')
+        plt.show()
     plt.close(fig)
