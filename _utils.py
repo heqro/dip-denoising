bcc96364754e7faa4ec5b8c5334a199b341f11b3
Héctor Iglesias <hr.iglesias.2018@alumnos.urjc.es>
2023-07-10 19:22:00 +0200

    Add Rician noise

--- _utils.py
+++ _utils.py
@@ -1,5 +1,7 @@
 import torch
 import torchvision
+import numpy as np
+from numpy import ndarray
 
 def __load_image__(path: str, mode: torchvision.io.ImageReadMode):
     return torchvision.io.read_image(path, mode) / 255
@@ -16,18 +18,27 @@ def crop_image(img, d=32):
     return img[:, :new_height, :new_width]
 
 def add_gaussian_noise(img, avg: float, std: float) -> torch.Tensor:
-    import numpy as np
     return img + np.random.normal(avg, std, img.shape).astype('float32')
 
+def add_rician_noise(img, std: float) -> torch.Tensor:
+    x_center, y_center = 0, 0
+    sample_size = img.shape[0] * img.shape[1] * img.shape[2]
+    distribution = np.random.normal(loc=[x_center, y_center], scale=std, size=(sample_size, 2))
 
-def print_image(img: torch.Tensor, file_name: str = "", show=False):
-    import matplotlib.pyplot as plt
+    noise_vector = np.linalg.norm(distribution, axis=1)
+
+    return img + noise_vector.reshape(img.shape).astype('float32')
+
+def print_image(img: ndarray, file_name: str = ""):
     img_transposed = img.transpose(1, 2, 0)
-    fig = plt.figure()
     if file_name != "":
-        plt.imsave(fname=file_name, arr=img_transposed, format="png")
-    else:
+        import imageio
+        imageio.imwrite(uri=file_name, im=img_transposed)
+    else: # will only work with RGB images
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
         plt.imshow(img_transposed)
         plt.axis('off')
         plt.show()
-    plt.close(fig)
+        plt.close(fig)
+    
