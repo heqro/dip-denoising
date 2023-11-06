59e0349dec8c2af17fd1cc423e21a692e4e7c6c8
Héctor Iglesias <hr.iglesias.2018@alumnos.urjc.es>
2023-11-06 17:35:59 +0100

    Tweak Rician noise

--- _utils.py
+++ _utils.py
@@ -13,21 +13,41 @@ def load_gray_image(path: str):
     return __load_image__(path, torchvision.io.image.ImageReadMode.GRAY)
 
 def crop_image(img, d=32):
-    new_height = img.shape[1] - img.shape[1] % d
-    new_width = img.shape[2] - img.shape[2] % d
+    new_height = img.shape[0] - img.shape[0] % d
+    new_width = img.shape[1] - img.shape[1] % d
     return img[:, :new_height, :new_width]
 
 def add_gaussian_noise(img, avg: float, std: float) -> torch.Tensor:
     return img + np.random.normal(avg, std, img.shape).astype('float32')
 
-def add_rician_noise(img, std: float) -> torch.Tensor:
-    x_center, y_center = 0, 0
-    sample_size = img.shape[0] * img.shape[1] * img.shape[2]
-    distribution = np.random.normal(loc=[x_center, y_center], scale=std, size=(sample_size, 2))
+# def add_rician_noise(img, std: float) -> torch.Tensor:
+#     x_center, y_center = 0, 0
+#     sample_size = img.shape[0] * img.shape[1] * img.shape[2]
+#     distribution = np.random.normal(loc=[x_center, y_center], scale=std, size=(sample_size, 2))
 
-    noise_vector = np.linalg.norm(distribution, axis=1)
+#     noise_vector = np.linalg.norm(distribution, axis=1)
 
-    return img + noise_vector.reshape(img.shape).astype('float32')
+#     return img + noise_vector.reshape(img.shape).astype('float32')
+
+def add_rician_noise(u, sigma, kspace=False) -> torch.Tensor:
+    if kspace:
+        j = complex(0, 1)
+        j = np.asarray(j)[None, None, None, ...]
+        w, h, c = np.shape(u)
+        omega = w*h*c
+        scale = 1/(np.pi) #TODO justificar
+        n = 1
+        u_F = np.fft.fftn(u, s=[n*w, n*h], axes=(0, 1))
+        f_F = u_F + np.sqrt(omega*scale)*(sigma * np.random.randn(n*w, n*h, c) + j * sigma * np.random.randn(n*w, n*h, c))
+        i_f_F = np.fft.ifftn(f_F, s=[n*w, n*h], axes=(0, 1))
+        f = np.sqrt(np.real(i_f_F)**2 + np.imag(i_f_F)**2)
+        # f = np.real(i_f_F)
+        f = f[0:w, 0:h, :]
+    else:
+        f_real = u + sigma * np.random.randn(u.shape[0], u.shape[1], u.shape[2])
+        f_imag = sigma * np.random.randn(u.shape[0], u.shape[1], u.shape[2])
+        f = np.sqrt(f_real**2 + f_imag**2)
+    return f
 
 def print_image(img: ndarray, file_name: str = ""):
     img_transposed = img.transpose(1, 2, 0)
